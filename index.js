7db0f61cc7e0addb166939c252ed9bdbd7ffef0a
cmrivera <cmrivera0004@student.stcc.edu>
2020-09-08 20:10:28 -0400

    almost mvp just missing /login

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const usersRouter = require("./users/users-router");
+const restrict = require("./middleware/restrict");
 
 const server = express();
 const port = process.env.PORT || 5000;
@@ -10,13 +11,7 @@ const port = process.env.PORT || 5000;
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-server.use(
-  session({
-    resave: false, // avoid recreating sessions that have not changed
-    saveUninitialized: false, // comply with GDPR laws for setting cookies automatically
-    secret: "keep it secret, keep it safe", // cryptographically sign the cookie
-  })
-);
+server.use(cookieParser());
 
 server.use(usersRouter);
 server.use((err, req, res, next) => {
@@ -26,7 +21,20 @@ server.use((err, req, res, next) => {
     message: "Something went wrong",
   });
 });
+server.use("/users", restrict, usersRouter);
 
 server.listen(port, () => {
   console.log(`Running at http://localhost:${port}`);
 });
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "pulling from server" });
+});
+
+/*Client sends credentials.
+Server verify credentials.
+Server creates a session for the client.
+Server produces and sends back cookie.
+Client stores the cookie.
+Client sends cookie on every request.
+Server verifies that cookie is valid.
+Server provides access to resource.*/
